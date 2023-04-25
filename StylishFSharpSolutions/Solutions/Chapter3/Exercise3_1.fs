c13559f3cb48438b72e068d50d89a9aee82ff764
Grenkin1988 <kruglovgleb@gmail.com>
2023-04-25 12:36:18 +0200

    Initial Chapter 3

---
+++ StylishFSharpSolutions/Solutions/Chapter3/Exercise3_1.fs
@@ -0,0 +1,41 @@
+﻿namespace Solutions.Chapter3
+
+module Exercise3_1 =
+    type Delivery =
+        | AsBilling
+        | Physical of string
+        | Download
+
+    type BillingDetails = {
+        name : string
+        billing :  string
+        delivery : Delivery }
+
+    let tryDeliveryLabel (billingDetails : BillingDetails) =
+        match billingDetails.delivery with
+        | AsBilling -> 
+            billingDetails.billing |> Some
+        | Physical address -> 
+            address |> Some
+        | Download -> None
+        |> Option.map (fun address -> 
+            sprintf "%s\n%s" billingDetails.name address)
+
+    let deliveryLabels (billingDetails : BillingDetails seq) =
+        billingDetails
+        |> Seq.choose tryDeliveryLabel
+
+    let myOrder = {
+        name = "Kit Eason"
+        billing = "112 Fibonacci Street\nErehwon\n35813"
+        delivery = AsBilling }
+
+    let hisOrder = {
+        name = "John Doe"
+        billing = "314 Pi Avenue\nErewhon\n15926"
+        delivery = Physical "16 Planck Parkway\nErewhon\n62291" }
+
+    let herOrder = {
+        name = "Jane Smith"
+        billing = "9 Gravity Road\nErewhon\n80665" 
+        delivery = Download }
