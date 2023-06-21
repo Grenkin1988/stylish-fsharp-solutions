8a0498c5864c734b33d39023161832513fdd5c6b
Grenkin1988 <kruglovgleb@gmail.com>
2023-06-22 00:16:13 +0200

    5.2 + 6.1

---
+++ StylishFSharpSolutions/Chapter05/Exercise5_2.fs
@@ -0,0 +1,48 @@
+﻿module StylishFSharpSolutions.Chapter05.Exercise5_2
+
+open NUnit.Framework
+
+let extremes (s : seq<float>) =
+    let first = Seq.head s
+    s
+    |> Seq.fold (fun (minimum, maximum) next -> (min minimum next, max maximum next )) (first,first)
+
+let extremes' (s : seq<float>) =
+    s |> Seq.min,
+    s |> Seq.max
+
+[<TestFixture>]
+module Test =
+    open FsUnit
+
+    [<Test>]
+    let ExtremesTest () =
+        let input = [| 1.0; 2.3; 11.1; -5. |]
+        let expected = (-5., 11.1)
+        let actual = extremes input
+        actual |> should equal expected
+
+    open System
+    open System.Diagnostics
+
+    [<Test>]
+    let ExtremesPerfTest () =
+        let r = Random()
+        let big = Array.init 1_000_000 (fun _ -> r.NextDouble())
+        let sw = Stopwatch()
+        sw.Start()
+        let min, max = extremes big
+        sw.Stop()
+        (min, max) |> should not' (be null)
+        printf "min: %f  max: %f - time: %ims" min max sw.ElapsedMilliseconds
+
+    [<Test>]
+    let ExtremesPerfTest' () =
+        let r = Random()
+        let big = Array.init 1_000_000 (fun _ -> r.NextDouble())
+        let sw = Stopwatch()
+        sw.Start()
+        let min, max = extremes' big
+        sw.Stop()
+        (min, max) |> should not' (be null)
+        printf "min: %f  max: %f - time: %ims" min max sw.ElapsedMilliseconds
---
+++ StylishFSharpSolutions/Chapter06/Exercise6_1.fs
@@ -0,0 +1,44 @@
+﻿module StylishFSharpSolutions.Chapter06.Exercise6_1
+
+open NUnit.Framework
+open System
+
+type MeterValue =
+    | Standard of int
+    | Economy7 of Day:int * Night:int
+
+type MeterReading =
+    {
+        ReadingDate : DateTime
+        MeterValue : MeterValue
+    }
+
+let printDate (date : DateTime) = date.ToString("dd/MM/yyyy", Globalization.CultureInfo.InvariantCulture)
+
+let formatReading (reading) =
+    match reading with
+    | { ReadingDate = date; MeterValue = Standard reading } ->
+        sprintf "Your reading on: %s was %07i" (printDate date) reading
+    | { ReadingDate = date; MeterValue =  Economy7(Day=day; Night=night) } ->
+        sprintf "Your readings on: %s: Day: %07i Night: %07i" (printDate date) day night
+
+let reading1 = { ReadingDate = new DateTime(2019, 3, 23); MeterValue = Standard 12982 }
+let reading2 = { ReadingDate = new DateTime(2019, 2, 24); MeterValue = Economy7(Day=3432, Night=98218) }
+
+
+
+[<TestFixture>]
+module Test =
+    open FsUnit
+
+    [<Test>]
+    let formatReading_reading1Test () =
+        let actual = reading1 |> formatReading
+        let expected = "Your reading on: 23/03/2019 was 0012982"
+        actual |> should equal expected
+
+    [<Test>]
+    let formatReading_reading2Test () =
+        let actual = reading2 |> formatReading
+        let expected = "Your readings on: 24/02/2019: Day: 0003432 Night: 0098218"
+        actual |> should equal expected
