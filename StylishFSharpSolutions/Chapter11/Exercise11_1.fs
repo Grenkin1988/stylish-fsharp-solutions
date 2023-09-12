95f97fcf5b860a89c441739bfd49d926d8ded265
Grenkin1988 <kruglovgleb@gmail.com>
2023-09-12 18:31:44 +0200

    Chapter11 completed

--- StylishFSharpSolutions/Chapter11/Exercise11_1.fs
+++ StylishFSharpSolutions/Chapter11/Exercise11_1.fs
@@ -1,1 +1,34 @@
-module StylishFSharpSolutions.Chapter11.Exercise11_1
+namespace StylishFSharpSolutions.Chapter11
+
+module Exercise11_01 =
+
+    type Outcome<'TSuccess, 'TFailure> =
+        | Success of 'TSuccess
+        | Failure of 'TFailure
+
+    let adapt func input =
+        match input with
+        | Success x -> func x
+        | Failure f -> Failure f
+
+    let passThrough func input =
+        match input with
+        | Success x -> func x |> Success
+        | Failure f -> Failure f
+
+    let passThroughRejects func input =
+        match input with
+        | Success x -> Success x
+        | Failure f -> func f |> Failure
+
+    open NUnit.Framework
+
+    [<TestFixture>]
+    module Test =
+        open FsUnit
+
+        [<Test>]
+        let ``Pass Through Rejects`` () =
+            let result = 2 |> Failure |> passThroughRejects (fun f -> f * 100)
+            let expected = 200 |> Failure
+            result |> should equal expected
---
+++ StylishFSharpSolutions/Chapter11/Exercise11_2.fs
@@ -0,0 +1,89 @@
+﻿namespace StylishFSharpSolutions.Chapter11
+
+module Exercise11_02 =
+
+    open System
+
+    type Message = 
+        { 
+            FileName : string
+            Content : float[] 
+        }
+
+    type Reading =
+        { 
+            TimeStamp : DateTimeOffset
+            Data : float[] 
+        }
+
+    let example =
+        [|
+            {   FileName = "2019-02-23T02:00:00-05:00"
+                Content = [|1.0; 2.0; 3.0; 4.0|] }
+            {   FileName = "2019-02-23T02:00:10-05:00"
+                Content = [|5.0; 6.0; 7.0; 8.0|] }
+            {   FileName = "error"
+                Content = [||] }
+            {   FileName = "2019-02-23T02:00:20-05:00"
+                Content = [|1.0; 2.0; 3.0; Double.NaN|] }
+        |]
+
+    let log s = printfn "Logging: %s" s
+
+    type MessageError =
+        | InvalidFileName of fileName:string
+        | DataContainsNaN of fileName:string * index:int
+
+    let getReading message =
+        let fileName = message.FileName
+        match DateTimeOffset.TryParse(fileName) with
+        | true, dt -> 
+            let reading = { TimeStamp = dt; Data = message.Content }
+            Ok (fileName, reading)
+        | false, _ -> 
+            fileName |> InvalidFileName |> Error
+
+    let validateData(fileName, reading) =
+        let nanIndex = 
+            reading.Data
+            |> Array.tryFindIndex (Double.IsNaN)
+        match nanIndex with
+        | Some i -> 
+            (fileName,i) |> DataContainsNaN |> Error
+        | None -> 
+            Ok reading
+
+    let logError (e : MessageError) =
+        match e with
+        | InvalidFileName fn ->
+            log (sprintf "Invalid file name: %s" fn)
+        | DataContainsNaN (fn, i) ->
+            log (sprintf "Data contains NaN at position: %i in file: %s" i fn)
+
+    open NUnit.Framework
+
+    [<TestFixture>]
+    module Test =
+        open FsUnit
+
+        [<Test>]
+        let ``Demo`` () =
+            let processMessage =
+                getReading
+                >> Result.bind validateData
+                >> Result.mapError logError
+
+            let processData data =
+                data
+                |> Array.map processMessage
+                |> Array.choose (fun result -> 
+                    match result with
+                    | Ok reading -> reading |> Some
+                    | Error _ -> None)
+            
+            let demo() =
+                example
+                |> processData
+                |> Array.iter (printfn "%A")
+            demo()
+
